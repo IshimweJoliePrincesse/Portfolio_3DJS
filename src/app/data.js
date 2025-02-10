3d5735ab98bce8fa1370f1e8219329fab8a9e3e8
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2025-02-10 15:55:19 +0200

    my new portfolio using 3Djs and frame motion

---
+++ src/app/data.js
@@ -0,0 +1,121 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Online Election System",
+    description: "Elect Online",
+    date: "2025-05-01",
+    demoLink: "https://github.com/IshimweJoliePrincesse/Vme",
+  },
+  {
+    id: 2,
+    name: "Health Dashboard",
+    description: "Digital dashboard for health ",
+    date: "2024-11-25",
+    demoLink: "https://github.com/IshimweJoliePrincesse/haealth_care-dashboard",
+  },
+  {
+    id: 3,
+    name: "Spotify Clone",
+    description: "Listen to your music on Spotify",
+    date: "2024-09-10",
+    demoLink: "https://github.com/IshimweJoliePrincesse/spotify_clone",
+  },
+  {
+    id: 4,
+    name: "Food delivery app",
+    description: "Order your food anytime you want",
+    date: "2024-05-30",
+    demoLink: "https://github.com/IshimweJoliePrincesse/Food_delivery-app",
+  },
+  {
+    id: 5,
+    name: "E-commerce app",
+    description: "Shop wherever ",
+    date: "2024-07-12",
+    demoLink: "https://github.com/IshimweJoliePrincesse/e_commrce",
+  },
+  {
+    id: 6,
+    name: "Real time chat app",
+    description: "Chat with your friends anytime you want to",
+    date: "2024-01-23",
+    demoLink: "https://github.com/IshimweJoliePrincesse/Real-Time-Chatapp",
+  },
+  {
+    id: 7,
+    name: "Library Management System",
+    description: "Library management system",
+    date: "2024-02-18",
+    demoLink: "https://github.com/IshimweJoliePrincesse/LMS",
+  },
+  {
+    id: 8,
+    name: "Umurava app",
+    description: "Skills Challenge App",
+    date: "2025-01-10",
+    demoLink: "https://umurava-combined-12m3.vercel.app/",
+  },
+  {
+    id: 9,
+    name: "Umurava Backend",
+    description: "Umurava Backend",
+    date: "2025-01-03",
+    demoLink: "https://umurava-swagger.onrender.com/api-docs",
+  },
+  {
+    id: 10,
+    name: "Site Blocker",
+    description: "Block any site you want with just this extension",
+    date: "2024-09-14",
+    demoLink: "https://github.com/IshimweJoliePrincesse/site_blocker",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/IshimweJoliePrincesse/",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/ishimwe-jolie-princesse-598b732a6/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://www.instagram.com/joly__princesse/",
+    icon: "instagram",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
---
+++ src/components/about/index.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            My journey in web development is powered by an array of mystical
+            tools and languages, with JavaScript casting the core of my
+            enchantments. I wield frameworks like React.js and Next.js with
+            precision, crafting seamless portals (websites) that connect realms
+            (users) across the digital universe. The ancient arts of the
+            Jamstack empower me to create fast, secure, and dynamic experiences,
+            while my design skills ensure every creation is not only functional
+            but visually captivating. Join me as I continue to explore new
+            spells and technologies to shape the future of the web.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            5+ <sub className="font-semibold text-base">clients</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            3+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src="https://avatars.githubusercontent.com/u/148842543?v=4"
+            alt="IshimweJoliePrincesse"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-[800px] h-[700px]"
+            src="https://i.pinimg.com/736x/a0/b8/1c/a0b81c08f4f0b0ed66673b3cd0225191.jpg"
+            alt="IshimweJoliePrincesse"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
+            alt="IshimweJoliePrincesse"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-[800px] h-[700px]"
+            src="https://i.pinimg.com/736x/a0/b8/1c/a0b81c08f4f0b0ed66673b3cd0225191.jpg"
+            alt="IshimweJoliePrincesse"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/IshimweJoliePrincesse/umurava-combined"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src="https://avatars.githubusercontent.com/u/148842543?v=4"
+              alt="IshimweJoliePrincesse"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
